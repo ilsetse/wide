4b5c3bed12d1e63b1c65b4902f4c73fd204f2317
An <kcj.pop.com@gmail.com>
2018-10-27 14:28:47 +0300

    Add a plot chart
    Signed-off-by: An <kcj.pop.com@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {Pane, Heading, Text} from 'evergreen-ui';
 import KeywordInput from './components/KeywordInput';
-import UniversityList from './components/UniversityList';
 import KeywordChart from './components/KeywordChart';
 
 class App extends React.Component {
@@ -19,25 +18,13 @@ class App extends React.Component {
             <Heading size={600} letterSpacing="2px">
               WIDE CHALLENGE
             </Heading>
-            <Text size={300}>
-              See Finnish performance on intertesting keywords
-            </Text>
           </Pane>
           <Pane width="75%">
             <KeywordInput />
           </Pane>
         </Pane>
 
-        <Pane display="flex">
-          <Pane width="25%" padding={16}>
-            <Heading is="h3" size={600} marginBottom={16} letterSpacing="2px">
-              Customize
-            </Heading>
-          </Pane>
-          <Pane width="75%" paddingLeft={16} paddingRight={16}>
-            <KeywordChart />
-          </Pane>
-        </Pane>
+        <KeywordChart />
       </div>
     );
   }
--- frontend/src/components/KeywordChart.js
+++ frontend/src/components/KeywordChart.js
@@ -1,40 +1,9 @@
 import React from 'react';
-import BubbleChart from '@weknow/react-bubble-chart-d3';
+import Plot from 'react-plotly.js';
+import DATA from './data';
+import DataProcessor from './DataProcessor';
 
 export default function KeywordChart(props) {
-  return (
-    <BubbleChart
-      graph={{
-        zoom: 0.5,
-        offsetX: -0.4,
-      }}
-      width={1000}
-      height={600}
-      showLegend={true} // optional value, pass false to disable the legend.
-      legendPercentage={10} // number that represent the % of with that legend going to use.
-      legendFont={{
-        size: 12,
-        color: '#000',
-        weight: 'bold',
-      }}
-      valueFont={{
-        size: 12,
-        color: '#fff',
-        weight: 'bold',
-      }}
-      labelFont={{
-        size: 16,
-        color: '#fff',
-        weight: 'bold',
-      }}
-      data={[
-        {label: 'tekoäly', value: 1},
-        {label: 'koneoppiminen', value: 1},
-        {label: 'Machine learning', value: 1},
-        {label: 'AI', value: 3},
-        {label: 'Deep learning', value: 5},
-        {label: 'Neural networks', value: 6},
-      ]}
-    />
-  );
+  const processed = DataProcessor(DATA);
+  return <Plot {...processed} />;
 }
