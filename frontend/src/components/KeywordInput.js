1cf93d38a39952a87f790580a895ba2588ec036e
An <kcj.pop.com@gmail.com>
2018-10-27 21:33:42 +0300

    App holds state of keywords
    Signed-off-by: An <kcj.pop.com@gmail.com>

--- frontend/src/components/KeywordInput.js
+++ frontend/src/components/KeywordInput.js
@@ -1,20 +1,16 @@
 import React from 'react';
-import Component from '@reactions/component';
 import {TagInput} from 'evergreen-ui';
 
 export default function KeywordInput(props) {
   return (
-    <Component initialState={{values: ['kertomukset', 'pakinat']}}>
-      {({state, setState}) => (
-        <TagInput
-          width="100%"
-          height={40}
-          values={state.values}
-          onChange={(values) => {
-            setState({values});
-          }}
-        />
-      )}
-    </Component>
+    <TagInput
+      width="100%"
+      height={40}
+      inputProps={{
+        placeholder: 'Enter a subject, for example: EEG',
+      }}
+      values={props.values}
+      onChange={props.onChange}
+    />
   );
 }
