4ccac6eec91f6924bb62952b1faa25cdc172df81
Ihsara <longchau21@gmail.com>
2018-10-28 03:35:49 +0200

    Add methods for query2

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,6 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources=r'/query/*')
+CORS(app, resources=[r'/query/*',r'/query2/*'])
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import app
 from flask import jsonify, request
 from scripts.get_org_size_and_comat import *
+from scripts.data_handling import *
 
 @app.route('/query/<beginYear>/<endYear>/<keyword>')
 def query(beginYear, endYear, keyword):
@@ -20,5 +21,28 @@ def query(beginYear, endYear, keyword):
         "comat" : comat
     }
     return jsonify(output)
+
+@app.route('/query2/<beginYear>/<endYear>/<keyword>')
+def query2(beginYear, endYear, keyword):
+    for year in range(int(beginYear) , int(endYear)):
+        result = get_and_beautify_data_from_api(lookfor=keyword, year=year)
+        if result['records'][0] != 0:
+            orgs_count = count_orgs(result['records'])#for bubble chart
+            subjects_count = count_subjects(result['records'])
+            cooccurence_with_keys = count_subjects_presented_together_one_publishcation(result['records'], subjects_count)
+            cooc_matrix = cooccurence_matrix(cooccurence_with_keys)#For heatmap chart
+            print("Organizations: {}\nRelated subjects: {}".format(orgs_count, list(subjects_count.keys())))
+        else:
+            orgs_count  = [0]
+            #cooc_matrix = [0]
+            cooccurence_with_keys = [0]
+            print("No result!!!!!")
+
+        output = {
+            "bubble": orgs_count,
+            "commat": cooccurence_with_keys
+        }
+
+        return jsonify(output)
 
 
